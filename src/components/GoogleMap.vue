658c1ee2054b3290f623f7d315de0fb55f577a85
x-ves <56628945+x-ves@users.noreply.github.com>
2020-09-14 13:10:40 +0300

    Add marker for Vikings Village

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -11,6 +11,14 @@ export default {
             const google = await gmapsInit();
             const geocoder = new google.maps.Geocoder();
             const map = new google.maps.Map(this.$el);
+            const locations = [
+                {
+                    position: {
+                        lat: 59.222549,
+                        lng: 25.032600
+                    }
+                }
+            ];
 
             geocoder.geocode({ address: "Viikingite küla" }, (results, status) => {
                 if (status !== "OK" || !results[0]) {
@@ -20,6 +28,11 @@ export default {
                 map.setCenter(results[0].geometry.location);
                 map.fitBounds(results[0].geometry.viewport);
             });
+
+            const marker = locations
+            .map (x => new google.maps.Marker({ ...x, map }));
+
+            marker;
         } catch (error) {
             console.error(error);
         }
